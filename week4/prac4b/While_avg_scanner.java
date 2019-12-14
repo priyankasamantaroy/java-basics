e7f49203ca7b20648c7fb253cdf82e73353ea8fa
priyankasamantaroy <priyanka.samantaroy@gmail.com>
2019-12-14 15:02:11 +0000

    My new file to add

---
+++ week4/prac4b/While_avg_scanner.java
@@ -0,0 +1,41 @@
+//Prac:4a
+//Q:8
+/*9.	Write a program using a while loop that will ask a user to enter any 10 numbers and 
+which will then calculate and display the average of the 10 numbers*/
+
+import java.util.*;
+public class While_avg_scanner
+{
+   public static void main(String args[])
+   {
+      //creating instance of scanner class
+      Scanner keyboardIn = new Scanner(System.in);
+      //creating variales
+      int inputnumber;
+      int sum=0;//initialising value
+      double avg;
+      int counter=1;//initialising value
+      //assign values for keyboard
+      //System.out.print("Enter any number:");
+      //inputnumber = keyboardIn.nextInt();
+      //applying while loop
+      while(counter<=9)
+      {              
+         System.out.println("Enter any number:" + counter);
+         inputnumber = keyboardIn.nextInt();
+         sum = sum + inputnumber;
+         counter++;
+      }
+         
+         avg = sum/counter;
+         System.out.print("Average is:"+ avg +" Sum is:"+sum + " times you added:" + counter);
+    }
+ }
+ 
+ 
+ 
+
+
+         
+      
+   
